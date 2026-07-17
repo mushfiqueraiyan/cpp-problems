98b1bb4ca239f762b9a11ee6573520e98f147623
Mushfique Raiyan <mushfiqueraiyan1@gmail.com>
2026-07-17 23:55:11 +0600

    Solved Bubble and Selection Sorting problems

---
+++ cpp practice/SelectionSort.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void selectionSorting(int arr[], int n){
+    for (int i = 0; i < n - 1; i++){
+        int min = i;
+        for (int j  = i + 1; j < n; j++){
+            if (arr[j] < arr[min]){
+                min = j;
+            }
+        }
+
+        swap(arr[i], arr[min]);
+    }
+}
+
+int main(){
+
+    int arr[] = {5,3,8,4,2};
+    int n  = 5;
+
+    selectionSorting(arr, n);
+
+    for(int x : arr){
+        cout << x << " ";
+    }
+    
+}
